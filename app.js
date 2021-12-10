2c6da9ecd3e5dc516edf085ad43fea340cea8e4c
heltonbc <94815542+heltonbc@users.noreply.github.com>
2021-12-10 18:46:31 -0400

    introducao do JS

---
+++ app.js
@@ -0,0 +1,11 @@
+const buttonElement = document.getElementById('btn');
+
+buttonElement.addEventListener('click', function (event) {
+    document.getElementById("nome").innerHTML = "A Imagem foi aberta em uma nova Aba!"
+
+    document.getElementById("btn").innerHTML;
+
+
+
+    
+  });
